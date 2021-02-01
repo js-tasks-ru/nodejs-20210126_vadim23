ffd4c3f08ce9827d86a788c08bf10c6a2d69967a
Vadim lisitcyn <pirat5gt@yandex.ru>
2021-02-01 20:16:45 +0300

    add solution for 01-intro 01-sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,10 @@
+const isNumber = (number) => typeof number === 'number' && isFinite(number)
+
 function sum(a, b) {
-  /* ваш код */
+    if (!isNumber(a) || !isNumber(b)) {
+        throw new TypeError()
+    }
+    return a + b
 }
 
 module.exports = sum;
