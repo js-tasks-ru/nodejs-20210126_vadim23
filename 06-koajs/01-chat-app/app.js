d4ef683101e20e00ff46a888a8b8e21fa5b36fa7
Vadim <vadim@lisicyn.ru>
2021-02-17 18:25:20 +0300

    add solution koajs

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,36 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/subscribe', async (ctx, next) => {
+let subscribers = {};
+
+const subscribe = (ctx) => {
+    return new Promise((resolve) => {
+        const id = Math.random();
+        subscribers[id] = ctx;
+        ctx.res.on("close", function() {
+            resolve();
+            delete subscribers[id];
+        });
+    })
+}
+
+router.get('/subscribe', async (ctx) => {
+    await subscribe(ctx);
 });
 
-router.post('/publish', async (ctx, next) => {
+router.post('/publish', async (ctx) => {
+
+    const message = ctx.request?.body?.message;
+
+    if (message) {
+        for (let id in subscribers) {
+            subscribers[id].status = 200;
+            subscribers[id].res.end(message);
+        }
+        subscribers = {};
+        ctx.status = 200;
+        ctx.res.end();
+    }
 });
 
 app.use(router.routes());
